cc26352d49b375a77f75502002566e558fb18859
Gary Smith <garysmith193358@gmail.com>
2024-06-20 18:09:27 -0400

    implemented functionality for user to input guess and have it be judged based on correct cows and bulls

--- src/bullscows/Main.java
+++ src/bullscows/Main.java
@@ -1,14 +1,62 @@
 package bullscows;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
+    public static String[] answer = {"4", "1", "7", "5"};
+    public static int cowCount = 0;
+    public static int bullCount = 0;
+
+
+    public static void checkDigits(String userGuess) {
+        for (int i = 0; i < userGuess.length(); i++) {
+            String num = String.valueOf(userGuess.charAt(i));
+
+            if (Objects.equals(num, answer[i])) { // bull
+                bullCount += 1;
+            }
+
+            else if (isCow(num)) { // cow
+                cowCount += 1;
+            }
+        }
+    }
+
+    public static boolean isCow(String numString) {
+
+        for (String value : answer) {
+            if (Objects.equals(value, numString)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void gradeAnswer() {
+
+        String answerString = String.join("", answer);
+        System.out.println(answerString);
+
+        if (bullCount > 0 && cowCount == 0) {
+            System.out.println("Grade " + bullCount + " bull(s). The secret code is " + answerString);
+        }
+
+        else if (cowCount > 0 && bullCount == 0){
+            System.out.println("Grade " + cowCount + " cow(s). The secret code is " + answerString);
+        }
+
+        else {
+            System.out.println("Grade: None. The secret code is " + answerString);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("The secret code is prepared: ****.");
-        System.out.println("Turn 1. Answer:");
-        System.out.println("1234");
-        System.out.println("Grade: None.");
-        System.out.println("Turn 2. Answer:");
-        System.out.println("9876");
-        System.out.println("Grade: 4 bulls.");
-        System.out.println("Congrats! The secret code is 9876.");
+        Scanner scanner = new Scanner(System.in);
+        String userGuess = scanner.nextLine();
+
+        checkDigits(userGuess);
+        gradeAnswer();
     }
 }
